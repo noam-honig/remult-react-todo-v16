96d2c50a1e47f05dbbb70657a6156f83327f8d88
Noam Honig <noam.honig@gmail.com>
2022-08-28 14:40:37 +0300

    todo app tutorial

---
+++ src/server/api.ts
@@ -0,0 +1,27 @@
+import { remultExpress } from 'remult/remult-express';
+import { Task } from '../shared/Task';
+import { remult } from 'remult';
+import { TasksController } from '../shared/TasksController';
+import { createPostgresConnection } from "remult/postgres";
+
+export const api = remultExpress({
+    dataProvider: process.env["NODE_ENV"] === "production" ?
+        createPostgresConnection({
+            configuration: "heroku"
+        }) : undefined,
+    entities: [Task],
+    controllers: [TasksController],
+    getUser: request => request.session!['user'],
+    initApi: async () => {
+        const taskRepo = remult.repo(Task);
+        if (await taskRepo.count() === 0) {
+            await taskRepo.insert([
+                { title: "Task a" },
+                { title: "Task b", completed: true },
+                { title: "Task c" },
+                { title: "Task d" },
+                { title: "Task e", completed: true }
+            ]);
+        }
+    }
+});
